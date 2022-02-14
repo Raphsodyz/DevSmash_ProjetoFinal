c2ae3e235194b01a90b1cabf4326dea97e4ced63
John DoeRaphsodyz <arthurvsr@gmail.com>
2022-02-13 23:38:35 -0300

    Adições front painel adm e listas CRUD

--- DevSmash - Projeto Final/wwwroot/js/site.js
+++ DevSmash - Projeto Final/wwwroot/js/site.js
@@ -1,1 +1,6 @@
-﻿
+﻿function MostrarLinha() {
+    var linha = document.getElementsByClassName("table-view");
+    for (var i = 0; i < linha.length; i++) {
+        linha[i].classList.toggle('ativar');
+    }
+}
